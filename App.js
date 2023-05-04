6b2e2bec755cad732c502262bbd0ac4741d41c06
Tanveer Bassi <tanveer.bassi@bbc.co.uk>
2023-05-04 01:32:00 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+} from "react-native";
+import { characters } from "./data";
 
 export default function App() {
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.character}
+      onPress={() => setSelectedCharacter(item)}
+    >
+      <Image style={styles.characterImage} source={item.image} />
+      <Text style={styles.characterName}>{item.name}</Text>
+    </TouchableOpacity>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Street Fighter 2 Character Select</Text>
+        <FlatList
+          data={characters}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={3}
+          contentContainerStyle={styles.characterList}
+        />
+        {selectedCharacter && (
+          <View style={styles.selectedCharacter}>
+            <Text style={styles.selectedCharacterText}>
+              Selected Character: {selectedCharacter.name}
+            </Text>
+          </View>
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  characterList: {
+    alignSelf: "center",
+  },
+  character: {
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+  },
+  characterImage: {
+    width: 100,
+    height: 225,
+    marginTop: 200,
+  },
+  characterName: {
+    marginTop: 5,
+  },
+  selectedCharacter: {
+    marginTop: 20,
+  },
+  selectedCharacterText: {
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
---
+++ data.js
@@ -0,0 +1,6 @@
+// data.js
+export const characters = [
+  { id: 1, name: "Ryu", image: require("./assets/ryu.png") },
+  { id: 2, name: "Ken", image: require("./assets/ken.jpg") },
+  // Add more characters here
+];
